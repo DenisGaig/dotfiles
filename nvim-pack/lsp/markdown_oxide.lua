936c5099ff1b7a8092f08e4445c17ecb725fc26a
Denis <denis_gaignard@yahoo.fr>
2026-05-31 04:06:57 +0200

    fix: probleme de mini-files - LSP activé sur filetype oubliés

--- nvim-pack/lsp/markdown_oxide.lua
+++ nvim-pack/lsp/markdown_oxide.lua
@@ -1,11 +1,5 @@
 -- lsp/markdown_oxide.lua
 return {
     cmd = { "markdown-oxide" },
-    capabilities = {
-        workspace = {
-            didChangeWatchedFiles = {
-                dynamicRegistration = true,
-            },
-        },
-    },
+    filetypes = { "markdown", "mdx" },
 }
