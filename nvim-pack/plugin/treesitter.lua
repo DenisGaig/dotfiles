b0c5e13b9d78bbbf5d56cfbba343a8ae91a9df0c
Denis <denis_gaignard@yahoo.fr>
2026-05-31 02:29:16 +0200

    feat: fisrt add

--- nvim-pack/plugin/treesitter.lua
+++ nvim-pack/plugin/treesitter.lua
@@ -1,59 +1,59 @@
-local add = require('vim-pack').add
-local on_plugin_update = require('vim-pack').on_plugin_update
+local add = require("vim-pack").add
+local on_plugin_update = require("vim-pack").on_plugin_update
 
 local parsers = {
-    'astro',
-    'bash',
-    'c',
+    "astro",
+    "bash",
+    "c",
     --'cpp',
-    'css',
-    'fish',
-    'gitcommit',
+    "css",
+    "fish",
+    "gitcommit",
     --'go',
     --'graphql',
-    'html',
-    'hyprlang',
+    "html",
+    "hyprlang",
     --'java',
-    'javascript',
-    'json',
-    'json5',
-    'lua',
-    'markdown',
-    'markdown_inline',
-    'mdx',
-    'python',
-    'query',
-    'rasi',
-    'regex',
+    "javascript",
+    "json",
+    "json5",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "mdx",
+    "python",
+    "query",
+    "rasi",
+    "regex",
     --'rust',
-    'scss',
-    'toml',
-    'tsx',
-    'typescript',
-    'vim',
-    'vimdoc',
-    'yaml',
+    "scss",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "yaml",
 }
 
 -- Highlight, edit, and navigate code.
 add {
     {
-        src = 'nvim-treesitter/nvim-treesitter',
+        src = "nvim-treesitter/nvim-treesitter",
         on_setup = function()
             -- Main-branch nvim-treesitter ships queries under `runtime/queries/`,
             -- which isn't on rtp by default. Prepend it so highlights/folds/indents
             -- are visible to `vim.treesitter.start`.
-            local init = vim.api.nvim_get_runtime_file('lua/nvim-treesitter/init.lua', false)[1]
+            local init = vim.api.nvim_get_runtime_file("lua/nvim-treesitter/init.lua", false)[1]
             if init then
-                vim.opt.runtimepath:prepend(vim.fn.fnamemodify(init, ':h:h:h') .. '/runtime')
+                vim.opt.runtimepath:prepend(vim.fn.fnamemodify(init, ":h:h:h") .. "/runtime")
             end
 
-            require('nvim-treesitter').install(parsers):wait(300000)
+            require("nvim-treesitter").install(parsers):wait(300000)
         end,
     },
     {
-        src = 'nvim-treesitter/nvim-treesitter-context',
-        module_name = 'treesitter-context',
+        src = "nvim-treesitter/nvim-treesitter-context",
+        module_name = "treesitter-context",
         opts = {
             -- Avoid the sticky context from growing a lot.
             max_lines = 3,
@@ -63,24 +63,34 @@ add {
             min_window_height = 20,
         },
         on_setup = function()
-            vim.keymap.set('n', '[c', function()
+            vim.keymap.set("n", "[c", function()
                 -- Jump to previous change when in diffview.
                 if vim.wo.diff then
-                    return '[c'
+                    return "[c"
                 else
                     vim.schedule(function()
-                        require('treesitter-context').go_to_context()
+                        require("treesitter-context").go_to_context()
                     end)
-                    return '<Ignore>'
+                    return "<Ignore>"
                 end
-            end, { desc = 'Jump to upper context', expr = true })
+            end, { desc = "Jump to upper context", expr = true })
+
+            vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+                pattern = { "mdx", "markdown" },
+                callback = function()
+                    local lang = "markdown"
+                    if not vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] then
+                        vim.treesitter.start(0, lang)
+                    end
+                end,
+            })
         end,
     },
 }
 
-on_plugin_update('nvim-treesitter', function()
+on_plugin_update("nvim-treesitter", function()
     -- Re-install (picks up any newly-added parsers from the list above)
     -- and update existing ones.
-    require('nvim-treesitter').install(parsers):wait(300000)
-    require('nvim-treesitter').update():wait(300000)
+    require("nvim-treesitter").install(parsers):wait(300000)
+    require("nvim-treesitter").update():wait(300000)
 end)
