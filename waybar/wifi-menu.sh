7a4b1a4b797af2ba4c0a36e724141925f8919e9d
Denis <denis_gaignard@yahoo.fr>
2026-05-31 22:26:51 +0200

    fix: prise en charge Galaxy pour le réseau

--- waybar/wifi-menu.sh
+++ waybar/wifi-menu.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Liste les réseaux disponibles et permet de s'y connecter via rofi
 
-#SELECTED=$(nmcli --fields SSID,SIGNAL,SECURITY -t device wifi list --rescan no | \
-#  awk -F: '{printf "%s\t(%s%%) %s\n", $1, $2, $3}' | \
-#  rofi -dmenu -p "󰤨  WiFi" -theme ~/.config/rofi/wifi-menu.rasi | \
-#  awk '{print $1}')
-
 SELECTED=$(nmcli -t -f SSID,SIGNAL,SECURITY device wifi list | \
   awk -F: '{printf "%s\t(%s%%) %s\n", $1, $2, $3}' | \
   rofi -dmenu -p "󰤨  WiFi" -theme ~/.config/rofi/wifi-menu.rasi | \
-  awk '{print $1}')
+  awk -F'\t' '{print $1}')
 
+# Si l'utilisateur annule (Escape) dans rofi, on quitte immédiatement
+if [ -z "$SELECTED" ]; then
+  exit 0
+fi
 
 # Vérifier si déjà connu
 KNOWN=$(nmcli -t -f NAME connection show | grep -x "$SELECTED")
@@ -20,4 +19,9 @@ if [ -n "$KNOWN" ]; then
 else
   PASSWORD=$(rofi -dmenu -p "󰌾  Mot de passe" -password -theme ~/.config/rofi/wifi-menu.rasi)
   nmcli device wifi connect "$SELECTED" password "$PASSWORD"
+
+  # Si l'utilisateur annule la saisie du mot de passe
+  if [ -z "$PASSWORD" ]; then
+    exit 0
+  fi
 fi
