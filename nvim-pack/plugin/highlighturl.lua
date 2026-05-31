755930a359f1dfb69f89b85e04a9150a4aa5381d
Denis <denis_gaignard@yahoo.fr>
2026-06-01 01:09:45 +0200

    feat: ajout url colorées

--- nvim-pack/plugin/highlighturl.lua
+++ nvim-pack/plugin/highlighturl.lua
@@ -1,17 +1,17 @@
-local add_on_event = require('vim-pack').add_on_event
+local add_on_event = require("vim-pack").add_on_event
 
 -- Highlight URLs.
-add_on_event('UIEnter', {
+add_on_event("UIEnter", {
     {
-        src = 'itchyny/vim-highlighturl',
+        src = "itchyny/vim-highlighturl",
         -- Vimscript-only plugin: skip require/setup entirely.
-        setup = false,
+        setup = false, -- permet de créer le setup perso suivant on_setup
         on_setup = function()
             -- Disable the plugin in some places where the default highlighting is preferred.
-            vim.api.nvim_create_autocmd('FileType', {
-                desc = 'Disable URL highlights',
-                pattern = { 'minifiles', 'fff' },
-                command = 'call highlighturl#disable_local()',
+            vim.api.nvim_create_autocmd("FileType", {
+                desc = "Disable URL highlights",
+                pattern = { "minifiles", "fff" },
+                command = "call highlighturl#disable_local()",
             })
         end,
     },
