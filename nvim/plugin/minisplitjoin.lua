3167e91a49930c490c21e9da002efec45cf28e58
Denis <denis_gaignard@yahoo.fr>
2026-06-27 16:37:03 +0200

    feat: better code

--- nvim/plugin/minisplitjoin.lua
+++ nvim/plugin/minisplitjoin.lua
@@ -1,13 +1,16 @@
-local add = require('vim-pack').add
+local add = require("vim-pack").add
 
 -- Split/join blocks of code.
 add {
     {
-        src = 'nvim-mini/mini.splitjoin',
+        src = "nvim-mini/mini.splitjoin",
         opts = {
-            mappings = {
-                toggle = '<leader>cj',
-            },
+            mappings = { toggle = "<leader>cj" },
         },
     },
 }
+
+-- Uniquement pour le renommage des mappings
+vim.keymap.set({ "n", "x" }, "<leader>cj", function()
+    require("mini.splitjoin").toggle()
+end, { desc = "Toggle split-join" })
