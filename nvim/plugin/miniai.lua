2a9f843f676ef865a1c19f1c75d4730dd00e6fca
Denis <denis_gaignard@yahoo.fr>
2026-06-27 16:34:19 +0200

    feat: ajout fonctionnalité de base

--- nvim/plugin/miniai.lua
+++ nvim/plugin/miniai.lua
@@ -1,32 +1,37 @@
-local add_on_event = require('vim-pack').add_on_event
+local add_on_event = require("vim-pack").add_on_event
 
 -- Better text objects.
-add_on_event('BufReadPre', {
+add_on_event("BufReadPre", {
     {
-        src = 'nvim-treesitter/nvim-treesitter-textobjects',
+        src = "nvim-treesitter/nvim-treesitter-textobjects",
         setup = false,
     },
     {
-        src = 'nvim-mini/mini.ai',
+        src = "nvim-mini/mini.ai",
         opts = function()
-            local miniai = require 'mini.ai'
+            local miniai = require "mini.ai"
 
             return {
-                n_lines = 300,
-                -- TODO: Add more custom entries?
+                n_lines = 100,
+                -- todo: add more custom entries?
                 custom_textobjects = {
-                    f = miniai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+                    f = miniai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
                 },
-                -- Disable error feedback.
+                -- disable error feedback.
                 silent = true,
-                -- Don't use the previous or next text object.
-                search_method = 'cover',
+                search_method = "cover_or_next",
+                -- don't use the previous or next text object.
+                -- search_method = 'cover',
                 mappings = {
-                    -- Disable next/last variants.
-                    around_next = '',
-                    inside_next = '',
-                    around_last = '',
-                    inside_last = '',
+                    -- disable next/last variants.(config MariaSol)
+                    -- around_next = "",
+                    -- inside_next = "",
+                    -- around_last = "",
+                    -- inside_last = "",
+                    around_next = "an",
+                    inside_next = "in",
+                    around_last = "al",
+                    inside_last = "il",
                 },
             }
         end,
